7bdb755bdf754d443f8f6c20d667076acddc6aee
Gandi098 <102229198+Gandi098@users.noreply.github.com>
2022-09-11 10:00:15 +0530

    Update source.py

--- FallenRobot/modules/source.py
+++ FallenRobot/modules/source.py
@@ -7,7 +7,7 @@ from pyrogram import filters
 from FallenRobot import pbot as client, dispatcher, OWNER_USERNAME
 
 
-ANON = "https://telegra.ph/file/c3d745fe632ed0a918719.jpg"
+ANON = "https://telegra.ph/file/b2b4fee33e6c7d2a5651a.jpg"
 
 
 @client.on_message(filters.command(["repo", "source"]))
@@ -16,7 +16,7 @@ async def repo(client, message):
         photo=ANON,
         caption=f"""**ʜᴇʏ​ {message.from_user.mention()},\n\nɪ ᴀᴍ [{dispatcher.bot.first_name}](t.me/{dispatcher.bot.username})**
 
-**» ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ :** [ᴍʀ ʟᴜᴄᴋʏ](tg://user?id=1960744931)
+**» ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ :** [ᴍʀ ʟᴜᴄᴋʏ](tg://user?id=1808943146)
 **» ᴩʏᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{y()}`
 **» ʟɪʙʀᴀʀʏ ᴠᴇʀsɪᴏɴ :** `{o}` 
 **» ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{s}` 
@@ -28,11 +28,11 @@ async def repo(client, message):
             [
                 [
                     InlineKeyboardButton(
-                        "• ᴏᴡɴᴇʀ •", url=f"https://t.me/{OWNER_USERNAME}"
+                        "• BOTS •", url=f"https://t.me/vip_creators"
                     ),
                     InlineKeyboardButton(
-                        "• sᴏᴜʀᴄᴇ •",
-                        url="https://github.com/mrluckyxd/Lucky-Robot",
+                        "• REPO •",
+                        url="https://github.com/VIP-MANAGER",
                     ),
                 ]
             ]
